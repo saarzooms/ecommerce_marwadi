d051ef92174832e8e6c74057ebaefddfc3fbad1c
nidhidhinoja <92723784+nidhidhinoja@users.noreply.github.com>
2023-05-19 14:35:01 +0530

    html to laravel

---
+++ templates/first-template/storage/framework/views/7e020e37bb38def67dead7b58761d414.php
@@ -0,0 +1,3 @@
+<?php echo $__env->make('front-end.layouts.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php echo $__env->yieldContent('main-container'); ?>
+<?php echo $__env->make('front-end.layouts.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\internship\laravel\first-template\resources\views/front-end/layouts/main.blade.php ENDPATH**/ ?>
